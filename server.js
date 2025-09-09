fe53590637213a214e43ecb207e01d2a88312c9a
nelyjc <nely.jazmin@outlook.com>
2025-09-08 21:17:51 -0600

    first changes

--- server.js
+++ server.js
@@ -6,25 +6,37 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
 /* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "layouts/layout") // not at view root
+/* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(require("./routes/static"))
+  
+// Index Route
+app.get("/", function(req, res) {
+  res.render("index", { title: "Home" })
+})
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT 
+const host = process.env.HOST 
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
-app.listen(port, () => {
+app.listen(port, host, () => {
   console.log(`app listening on ${host}:${port}`)
 })
