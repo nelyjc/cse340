f70908dc628c41d6f5026169adbb3a4678838db4
nelyjc <nely.jazmin@outlook.com>
2025-09-25 17:50:44 -0600

    teacm activity lesson 3

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -7,6 +7,7 @@ async function getClassifications(){
   return await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
 }
 
+
 module.exports = {getClassifications}
 
 /* ***************************
