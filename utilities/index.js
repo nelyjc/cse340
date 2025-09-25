b219b4a362d172a71ecc9ed21a3d57018e6a6f65
nelyjc <nely.jazmin@outlook.com>
2025-09-25 17:25:53 -0600

    week 3 learning activities done

--- utilities/index.js
+++ utilities/index.js
@@ -1,56 +1,111 @@
+// const invModel = require("../models/inventory-model")
+// const Util = {}
+
+// /* ************************
+//  * Constructs the nav HTML unordered list
+//  ************************** */
+// Util.getNav = async function (req, res, next) {
+//   let data = await invModel.getClassifications()
+//   console.log(data)
+//   let list = "<ul>"
+//   list += '<li><a href="/" title="Home page">Home</a></li>'
+//   data.rows.forEach((row) => {
+//     list += "<li>"
+//     list +=
+//       '<a href="/inv/type/' +
+//       row.classification_id +
+//       '" title="See our inventory of ' +
+//       row.classification_name +
+//       ' vehicles">' +
+//       row.classification_name +
+//       "</a>"
+//     list += "</li>"
+//   })
+//   list += "</ul>"
+//   return list
+// }
+
+// /* ****************************************
+//  * Middleware For Handling Errors
+//  * Wrap other function in this for 
+//  * General Error Handling
+//  **************************************** */
+// Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
+
+// module.exports.handleErrors = (fn) => {
+//   return async (req, res, next) => {
+//     try {
+//       await fn(req, res, next)
+//     } catch (err) {
+//       next(err) // passes the error to Express error handler
+//     }
+//   }
+// }
+
+
+// module.exports = Util
+
+
+
+// /* **************************************
+// * Build the classification view HTML
+// * ************************************ */
+// Util.buildClassificationGrid = async function(data){
+//   let grid
+//   if(data.length > 0){
+//     grid = '<ul id="inv-display">'
+//     data.forEach(vehicle => { 
+//       grid += '<li>'
+//       grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
+//       + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
+//       + 'details"><img src="' + vehicle.inv_thumbnail 
+//       +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
+//       +' on CSE Motors" /></a>'
+//       grid += '<div class="namePrice">'
+//       grid += '<hr />'
+//       grid += '<h2>'
+//       grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
+//       + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
+//       + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
+//       grid += '</h2>'
+//       grid += '<span>$' 
+//       + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
+//       grid += '</div>'
+//       grid += '</li>'
+//     })
+//     grid += '</ul>'
+//   } else { 
+//     grid += '<p class="notice">Sorry, no matching vehicles could be found.</p>'
+//   }
+//   return grid
+// }
 const invModel = require("../models/inventory-model")
 const Util = {}
 
-/* ************************
- * Constructs the nav HTML unordered list
- ************************** */
-Util.getNav = async function (req, res, next) {
-  let data = await invModel.getClassifications()
+// Build navigation list
+Util.getNav = async function () {
+  let data = await invModel.getClassifications() // ideally returns rows
   let list = "<ul>"
   list += '<li><a href="/" title="Home page">Home</a></li>'
   data.rows.forEach((row) => {
-    list += "<li>"
-    list +=
-      '<a href="/inv/type/' +
-      row.classification_id +
-      '" title="See our inventory of ' +
-      row.classification_name +
-      ' vehicles">' +
-      row.classification_name +
-      "</a>"
-    list += "</li>"
+    list += `<li><a href="/inv/type/${row.classification_id}" title="See our inventory of ${row.classification_name} vehicles">${row.classification_name}</a></li>`
   })
   list += "</ul>"
   return list
 }
 
-module.exports = Util
-
-
-
-/* **************************************
-* Build the classification view HTML
-* ************************************ */
-Util.buildClassificationGrid = async function(data){
-  let grid
+// Build classification grid
+Util.buildClassificationGrid = async function(data) {
+  let grid = ''
   if(data.length > 0){
-    grid = '<ul id="inv-display">'
+    grid += '<ul id="inv-display">'
     data.forEach(vehicle => { 
       grid += '<li>'
-      grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
-      + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
-      + 'details"><img src="' + vehicle.inv_thumbnail 
-      +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
-      +' on CSE Motors" /></a>'
+      grid += `<a href="../../inv/detail/${vehicle.inv_id}" title="View ${vehicle.inv_make} ${vehicle.inv_model} details"><img src="${vehicle.inv_thumbnail}" alt="Image of ${vehicle.inv_make} ${vehicle.inv_model} on CSE Motors" /></a>`
       grid += '<div class="namePrice">'
       grid += '<hr />'
-      grid += '<h2>'
-      grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
-      + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
-      + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
-      grid += '</h2>'
-      grid += '<span>$' 
-      + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
+      grid += `<h2><a href="../../inv/detail/${vehicle.inv_id}" title="View ${vehicle.inv_make} ${vehicle.inv_model} details">${vehicle.inv_make} ${vehicle.inv_model}</a></h2>`
+      grid += `<span>$${new Intl.NumberFormat('en-US').format(vehicle.inv_price)}</span>`
       grid += '</div>'
       grid += '</li>'
     })
@@ -60,3 +115,16 @@ Util.buildClassificationGrid = async function(data){
   }
   return grid
 }
+
+// Middleware for handling async errors
+Util.handleErrors = (fn) => {
+  return async (req, res, next) => {
+    try {
+      await fn(req, res, next)
+    } catch (err) {
+      next(err)
+    }
+  }
+}
+
+module.exports = Util
